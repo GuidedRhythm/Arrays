9d56bd15835d8096b7b012d61bd010f4a2d671a5
Studend@202-11 <Student@202-11>
2019-07-03 20:17:11 +0300

    Functions called

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 using namespace std;
 
+void FillRand(int Arr[], const int n);
+void Print(int Arr[], const int n);
+
 void main()
 {
 	setlocale(LC_ALL, "");
+	const int n = 5;
+	int Arr[n];
+	FillRand(Arr, n);
+	Print(Arr, n);
 }
 
 void FillRand(int Arr[], const int n)
