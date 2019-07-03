0a8c837cf954dc2f933bd52f1d216d5f768de344
student <1124@gmail.com>
2019-07-03 19:00:33 +0300

    Init commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "");
+}
+
+void FillRand(int Arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		Arr[i] = rand() % 100;
+	}
+}
+
+void Print(int Arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << Arr[i] << "\t";
+	}
+	cout << endl;
+}
